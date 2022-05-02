15d422989d35d2765f6c9f7428cbbbd98c130278
SaaberDev <saaberdev@gmail.com>
2022-05-03 04:49:58 +0600

    fixed icon and image in pdf

--- resources/views/admin_panel/pages/pdf_blades/preview_pdf.blade.php
+++ resources/views/admin_panel/pages/pdf_blades/preview_pdf.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="shortcut icon" href="{{ asset('admin_panel/assets/img/logo/favicon.png') }}" type="image/png">
     <title>{{ $assets->company_name }} - Final Report</title>
 
     <style>
